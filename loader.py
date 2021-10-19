e7f7192eae30c55526ab4099a97f79011ca9ae4c
pascal <22preich@shcp.edu>
2021-10-19 14:55:39 -0700

    Update loader.py

--- loader.py
+++ loader.py
@@ -5,7 +5,7 @@ from tf_agents.environments import tf_py_environment
 
 eval_env = tf_py_environment.TFPyEnvironment(agent.Game(limit=False))
 
-saved_policy = tf.compat.v2.saved_model.load('policies/epoch_run/policy_40000')
+saved_policy = tf.compat.v2.saved_model.load('perfect_policies/policy_5_width_max_score')
 time_step = eval_env.reset()
 while True:
     policy_step = saved_policy.action(time_step)
